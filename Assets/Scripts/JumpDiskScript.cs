60ee2182d08d13877d8c554e40a304bea4e9d9a6
Roman <r.tchirkov2010@yandex.ru>
2020-01-25 17:47:31 +0300

    Create JumpDiskScript.cs

---
+++ Assets/Scripts/JumpDiskScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpDiskScript : MonoBehaviour
+{
+    public bool playerEnter = false;
+    public int score;
+    private void OnTriggerEnter(Collider other) 
+    {
+        playerEnter = true;
+        score++;
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        playerEnter = false;    
+    }
+}
