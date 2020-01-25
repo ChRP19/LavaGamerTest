bc29e8ffa0360241d460222fa5ac7d451c97fbaf
Roman <r.tchirkov2010@yandex.ru>
2020-01-25 17:47:48 +0300

    Create PlayerMove.cs

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 10.0f;
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+}
