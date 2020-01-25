bd6911941e062305c046c9c2a7ec8fb176f67d31
Roman <r.tchirkov2010@yandex.ru>
2020-01-25 17:14:23 +0300

    Create comback script

---
+++ Assets/Scripts/CombackScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombackScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) 
+    {
+        other.gameObject.transform.position = new Vector3(-15, 1.1f, 0);        
+    }
+}
