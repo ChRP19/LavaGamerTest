82274cb47238f59ace9d590dd54aa8127de0e8e0
Roman <r.tchirkov2010@yandex.ru>
2020-01-25 17:47:24 +0300

    Update CombackScript.cs

--- Assets/Scripts/CombackScript.cs
+++ Assets/Scripts/CombackScript.cs
@@ -6,6 +6,6 @@ public class CombackScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other) 
     {
-        other.gameObject.transform.position = new Vector3(-15, 1.1f, 0);        
+        other.gameObject.transform.position = new Vector3(-15, 1.3f, 0);        
     }
 }
