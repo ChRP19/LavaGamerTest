d79095bdff1c08652d0b8eff98c09d627f4974e2
Roman <r.tchirkov2010@yandex.ru>
2020-01-25 17:47:40 +0300

    Create JumpScript.cs

---
+++ Assets/Scripts/JumpScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+    [SerializeField] private GameObject jumpDisk;
+    private Vector3 mousePos;
+    void Update()
+    {
+        if (jumpDisk.GetComponent<JumpDiskScript>().playerEnter && Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                mousePos = hit.point;
+            }
+
+            Debug.Log(mousePos.x);
+        }
+    }
+}
