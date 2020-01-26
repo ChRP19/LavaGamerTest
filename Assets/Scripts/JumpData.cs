3776d5437a3ba78959a14bcbc18942ffab15fd15
Roman <r.tchirkov2010@yandex.ru>
2020-01-26 22:12:08 +0300

    Create SO script

---
+++ Assets/Scripts/JumpData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class JumpData : ScriptableObject
+{
+    [SerializeField] private int jumpHeight;
+    [SerializeField] private Vector3 jumpDiskScale;
+    public int JumpHeight { get => jumpHeight;}
+    public Vector3 JumpDiskScale { get => jumpDiskScale; }
+}
